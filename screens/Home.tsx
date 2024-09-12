521fbb95c358a3e07c9248ff787a57552e5900cf
unknown <unekue2@gmail.com>
2024-09-12 10:45:05 +0100

    setup db

---
+++ screens/Home.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { useState } from "react";
+import { Text, View } from "react-native";
+
+export default function Home() {
+  const [categories, setCategories] = useState();
+  return (
+    <View>
+      <Text>hOMEsCREEN</Text>
+    </View>
+  );
+}
